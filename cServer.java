14a479efc71b49598b4c4a5de418fb6217fbeb99
Trần Trang Kỳ Phong <80328762+WindyFeg@users.noreply.github.com>
2022-11-23 22:57:21 +0700

    Cleanup code

--- cServer.java
+++ cServer.java
@@ -1,18 +1,10 @@
 import UI.*;
 
-/**
- * UI
- */
 public class cServer {
 
     public static void main(String[] args) 
     throws Exception
     {
-        //MyFrame f = new MyFrame();
-
-        //ClientUI clientUI = new ClientUI();
         SeverUI severUI = new SeverUI();
-        //ChatSever newSever = new ChatSever(0);
-        //newSever.listen(9888);
     }
 }
