175e570abb3857baef5ad698767a6eaa81560e96
Trần Trang Kỳ Phong <80328762+WindyFeg@users.noreply.github.com>
2022-11-23 22:56:53 +0700

    Cleanup code

--- cClient.java
+++ cClient.java
@@ -1,19 +1,10 @@
 import UI.*;
-/**
- * UI
- */
+
 public class cClient {
 
     public static void main(String[] args) 
     throws Exception
     {
-        //MyFrame f = new MyFrame();
-
         ClientUI clientUI = new ClientUI();
-        //SeverUI severUI = new SeverUI();
-        // ChatSever newSever = new ChatSever(0);
-        // newSever.listen(9888);
-        // ChatClient newclient = new ChatClient();
-        // newclient.Set("127.0.0.1", 9888);
     }
 }
